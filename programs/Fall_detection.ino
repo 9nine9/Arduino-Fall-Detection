81a3a0eb95ef8e1b8db2300de3662c59548c9a79
agjunyent <albertgaju@gmail.com>
2013-09-11 18:19:51 +0200

    v1 of the final program, need test

--- programs/Fall_detection.ino
+++ programs/Fall_detection.ino
@@ -1,29 +1,39 @@
-/*========================================
-Program that detects a fall ensuring that
-it's not a "false positive" (jumps, etc).
-========================================*/
+/*=================================================================================
+Program that detects a fall ensuring that it's not a "false positive" (jumps, etc).
+IF a fall is detected, it waits for 10 seconds before sending a message to the 
+receiver, when the user can press a button to cancel this operation in case of a
+"not fatal fall".
+=================================================================================*/
 
-/*========================================
-This library has all the functions used in
-the accelerometer MMA7361.
-========================================*/
+/*=================================================================================
+This library has all the functions used in the accelerometer MMA7361.
+=================================================================================*/
 
 #include <AcceleroMMA7361.h>
 
-/*========================================
-Declaration of all the variables used in
-the program.
-========================================*/
+/*=================================================================================
+This library has all the functions used in the RF communication.
+=================================================================================*/
+
+#include <VirtualWire.h>
+
+/*=================================================================================
+Declaration of all the variables used in the program.
+=================================================================================*/
 
 AcceleroMMA7361 accelero;
 float x, y, z;
-int xpin, ypin, zpin, gs, sl, g0, st;
-unsigned long start_time, falling_time;
-boolean falling;
+int xpin, ypin, zpin, gs, sl, g0, st, interrupt_pin_number;
+unsigned long start_falling_time, falling_time, start_message_time;
+boolean falling, send_message, fall;
+volatile boolean cancel;
 
 
 void setup()
 {
+  interrupt_pin_number = 2;
+  attatchInterrupt(interrupt_pin_number, cancel_send, RISING);
+
   /*Declare accelerometer pinout*/
   xpin = A0;   						//x axis pin
   ypin = A1;   						//y axis pin
@@ -32,9 +42,13 @@ void setup()
   sl = 4;    						//sleep pin: HIGH = ON, LOW = OFF
   g0 = 3;    						//0g: detects free fall (HIGH when all axis = 0)
   st = 6;    						//self-test pin
+
   falling = false;
+  send_message = false;
+  cancel = false;
 
   Serial.begin(9600);  
+  vw_setup(2000);
 
   /*Configurate accelerometer*/
   accelero.begin(sl, st, g0, gs, xpin, ypin, zpin); 
@@ -48,7 +62,7 @@ void loop()
   get_accelerations();
   if (x<0.1 && y<0.1 && z<0.1)                   	//if all three axis are less than 0.1g, it's falling!
   {
-    start_time = millis();
+    start_falling_time = millis();
     falling = true;
     while ((z<5 && y<5 && z<5) && falling)       	//wait for the "peak" in "g's" when it hits the ground
     {
@@ -59,7 +73,24 @@ void loop()
         falling = false;
       }
     }
-    if (falling) Serial.println("FALL!");
+    if (falling) 
+    {
+      fall = true;
+    }
+  }
+  if (fall)
+  {
+    wait_ten_seconds();
+  }
+}
+
+void wait_ten_seconds()
+{
+  start_message_time = millis();
+  while ((millis() - start_message_time)<10000 && !cancel);
+  if (!cancel)
+  {
+    send("FALL");
   }
 }
 
@@ -68,4 +99,18 @@ void get_accelerations()
   x = float(accelero.getXAccel())/100;
   y = float(accelero.getYAccel())/100;
   z = float(accelero.getZAccel())/100;
+}
+
+void send(char *message)
+{
+  vw_send((uint8_t *)message, strlen(message));
+  vw_wait_tx();                                         // Wait until the whole message is send
+}
+
+void cancel_send()
+{
+  if (falling)
+  {
+    cancel = true;
+  }
 }
